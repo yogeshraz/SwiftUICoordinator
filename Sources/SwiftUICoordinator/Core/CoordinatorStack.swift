306f88626bd06bcdbe09354f41f3dda86d9398fc
Yogesh Raj <yogeshraz@hotmail.com>
2025-03-31 22:07:44 +0530

    Update CoordinatorStack.swift

--- Sources/SwiftUICoordinator/Core/CoordinatorStack.swift
+++ Sources/SwiftUICoordinator/Core/CoordinatorStack.swift
@@ -19,11 +19,26 @@ public struct CoordinatorStack<CoordinatorViews: Coordinatable>: View {
     public var body: some View {
         NavigationStack(path: $coordinator.path) {
             root
-                .navigationDestination (for: CoordinatorViews.self) { $0 }
+                .navigationDestination (for: CoordinatorViews.self) { view in
+                    view.onDisappear() {
+                        detectBackNavigation()
+                    }
+                }
                 .sheet(item: $coordinator.sheet) { $0 }
                 .fullScreenCover(item: $coordinator.fullScreenCover) { $0 }
                 .showAlert(isShowing: $coordinator.isShowingAlert, details: coordinator.alertDetails)
         }
         .environment(coordinator)
+        .onChange(of: coordinator.path) { oldStack, newStack in
+            if newStack.count < oldStack.count {
+                coordinator.pop()
+                print("Back button tapped or swiped back")
+            }
+        }
+    }
+    
+    private func detectBackNavigation() {
+        coordinator.pop()
+        print("User navigated back via swipe or back button")
     }
 }
